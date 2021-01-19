6f2c7beefc2cf45db2faea677eaccaf8a7728193
gyenesmatyas <64597684+gyenesmatyas@users.noreply.github.com>
2021-01-19 12:00:39 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+function onze_aanpak_change_ocean(){
+	window.location.href = "index.html#onze_aanpak";
+
+}
+function onze_aanpak_change_river(){
+	window.location.href = "index_rivers.html#onze_aanpak_2";
+}
